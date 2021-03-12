fe52aa6e74add371fb6a938830e0581a2d358664
thegodone <guillaume.godin@gmail.com>
2021-03-12 08:05:27 +0000

    working minimal version

--- AttentiveFP/__init__.py
+++
@@ -1,3 +0,0 @@
-from AttentiveFP.featurizing import graph_dict
-from AttentiveFP.AttentiveLayers import Fingerprint, graph_dataset, null_collate, Graph
-from AttentiveFP.AttentiveLayers_geometric import  Logger, time_to_str
